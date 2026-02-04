89048b2851fb4100e314849788bdf54e7256e36a
Jakalliv <f.galdenzi@gmail.com>
2026-02-04 17:27:36 +0100

    hash table exercise

---
+++ freecodecamp/hash_table.py
@@ -0,0 +1,18 @@
+class HashTable:
+    def __init__(self):
+        self.collection = {}
+    
+    def hash(self, text: str):
+        return sum(ord(x) for x in text)
+    
+    def add(self, key, value):
+        if self.hash(key) in self.collection:
+            self.collection[self.hash(key)][key] = value
+        else:
+            self.collection[self.hash(key)] = {key: value}
+        
+
+variable = HashTable()
+variable.add("ciao", 10)
+variable.add("ciao", 12)
+
